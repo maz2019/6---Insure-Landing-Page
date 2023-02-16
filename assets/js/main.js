87c886fab228051d4af13c3e73ee07496e77c8c8
Mohamed Elazhary <azhary2@hotmail.com>
2023-02-16 22:50:10 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+const navbar = document.getElementById("navbar");
+const menubtn = document.getElementById("menu-btn");
+
+menubtn.addEventListener('click', ()=> {
+    navbar.classList.toggle("hidden")
+})
